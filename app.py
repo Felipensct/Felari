75d869a34a757d9d42f42363f3c4426d2b67468e
Felipe Nascimento <felipensct.silva@gmail.com>
2024-05-17 21:15:01 -0300

    atualização
    adicionei serial

--- app.py
+++ app.py
@@ -1,13 +1,25 @@
+import random
 from flask import Flask,render_template,url_for,request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, Length
+from flask import flash
+
+class TaskForm(FlaskForm):
+    content = StringField(validators=[DataRequired(), Length(min=5, max=200)])
+    submit = SubmitField('Submit')
  
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'root'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 migrate = Migrate(app,db)
+
+
 
 class aFazer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -19,53 +31,38 @@ class aFazer(db.Model):
     def __repr__(self):
         return '<Task %r>' % self.id
 
-@app.route('/', methods=['POST','GET'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method=='POST':
-        task_content = request.form['content']
-        new_task = aFazer(content = task_content)
-        
-
-        try:
-            db.session.add(new_task)
-            db.session.commit()
-            new_task.serial_number = f'AF-{new_task.id}'
-            db.session.commit()
-            return redirect('/')
-        except:
-            return 'Houve um erro'
-    else:
-        tasks = aFazer.query.order_by(aFazer.date_created).all()
-        return render_template('index.html', tasks = tasks)
-    
-
-@app.route('/delete/<int:id>')
-def delete(id):
-    task_to_delete = aFazer.query.get_or_404(id)
-
-    try:
-        db.session.delete(task_to_delete)
+    form = TaskForm()
+    if form.validate_on_submit():
+        new_task = aFazer(content=form.content.data)
+        new_task.serial_number = str(random.randint(100000, 999999))
+        db.session.add(new_task)
         db.session.commit()
+        flash('Tarefa adicionada com sucesso!')  # Mensagem flash
         return redirect('/')
-    except:
-        return 'Houve um erro'
+    tasks = aFazer.query.order_by(aFazer.date_created).all()
+    return render_template('index.html', form=form, tasks=tasks)
     
 
 @app.route('/update/<int:id>', methods=['GET', 'POST'])
 def update(id):
     task = aFazer.query.get_or_404(id)
+    form = TaskForm(obj=task)
+    if form.validate_on_submit():
+        task.content = form.content.data
+        db.session.commit()
+        return redirect('/')
+    return render_template('update.html', form=form, task=task)
 
-    if request.method == 'POST':
-        task.content = request.form['content']
-
-        try:
-            db.session.commit()
-            return redirect('/')
-        except:
-            return 'Houve um erro ao atualizar a tarefa'
-
-    else:
-        return render_template('update.html', task=task)
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = aFazer.query.get_or_404(id)
+    db.session.delete(task_to_delete)
+    db.session.commit()
+    flash('Tarefa deletada com sucesso!')  # Mensagem flash
+    return redirect('/')
+    
     
 
 if __name__ == '__main__':
